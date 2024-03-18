7f53a7eb464f438a61cab5aedeb41467b84139d0
alex-from-90 <alex@site-n.ru>
2024-03-18 14:08:12 +0300

    Update Java 21, telegrambots 6.9.7.1, spring 3.2.3

---
+++ src/test/java/ru/alex/botmydubai/BotMyDubaiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.botmydubai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotMyDubaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
